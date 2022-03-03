7daaacecb15916b07c9dc5c877f8828979bfdeeb
RahulFernando <rahulrulz680@gmail.com>
2022-03-03 15:13:18 +0530

    cart animation

--- src/components/Layout/Cart/CartButton.js
+++ src/components/Layout/Cart/CartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 // context
 import CartContext from '../../../store/cart-context';
@@ -9,14 +9,34 @@ import classes from './Cart.module.css';
 import CartIcon from './CartIcon';
 
 const CartButton = (props) => {
+  const [btnIsBump, setBtnIsBump] = useState(false);
   const cartContext = useContext(CartContext);
-  
+
+
+  const { items } = cartContext;
   const noOfItems = cartContext.items.reduce((current, item) => {
     return current + item.amount;
   }, 0)
 
+  const btnClasses = `${classes.button} ${btnIsBump ? classes.bump : ''}`;
+
+  useEffect(() => { 
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsBump(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.onCartShow}>
+    <button className={btnClasses} onClick={props.onCartShow}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
